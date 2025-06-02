164b0489683fd48aa3b23a25e24a992044fac95e
Victor Ejike Nwosu <ejikello123@yahoo.com>
2025-06-02 16:56:14 +0200

    Add Program class with Main method for HelloWorld application

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var greeting = new Greeting();
+            var greetingWithParams = new GreetingWithParams();
+            var greetingWithLambda = new GreetingWithLambda();
+
+            Console.WriteLine("Hello, World from JS world!");
+
+
+            greeting.Execute();
+            greetingWithParams.Execute("Alice", "Hernandez");
+            greetingWithParams.Execute("Micheal", "Deo");
+            greetingWithLambda.Execute("Mary", "Luke", static (greet) => Console.WriteLine(greet));
+
+        }
+    }
+}
