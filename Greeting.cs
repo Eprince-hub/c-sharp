6b324e568d3ec88c77aa0366fc02114b444f0133
Victor Ejike Nwosu <ejikello123@yahoo.com>
2025-06-09 20:36:08 +0200

    Refactor Greeting class structure for improved readability and organization

--- Greeting.cs
+++ Greeting.cs
@@ -7,29 +7,33 @@
 // public string firstName = "Victor";
 // public string lastName = "Hugo";
 
-public class Greeting
+namespace GreetingNameSpace
 {
 
-  const string firstName = "Victor";
-  const string lastName = "Hugo";
-  public void Execute()
+  public class Greeting
   {
-    Console.WriteLine($"My first name is {firstName} and my last name is {lastName}.");
-  }
-}
-public class GreetingWithParams
-{
-  public void Execute(string firstName, string lastName)
-  {
-    Console.WriteLine($"My first name is {firstName} and my last name is {lastName}.");
-  }
-}
 
-public class GreetingWithLambda
-{
-  public void Execute(string firstName, string lastName, Action<string> action)
+    const string firstName = "Victor";
+    const string lastName = "Hugo";
+    public void Execute()
+    {
+      Console.WriteLine($"My first name is {firstName} and my last name is {lastName}.");
+    }
+  }
+  public class GreetingWithParams
   {
-    Console.WriteLine($"My first name is {firstName} and my last name is {lastName}.");
-    action($"Hello {firstName} {lastName}!");
+    public void Execute(string firstName, string lastName)
+    {
+      Console.WriteLine($"My first name is {firstName} and my last name is {lastName}.");
+    }
+  }
+
+  public class GreetingWithLambda
+  {
+    public void Execute(string firstName, string lastName, Action<string> action)
+    {
+      Console.WriteLine($"My first name is {firstName} and my last name is {lastName}.");
+      action($"Hello {firstName} {lastName}!");
+    }
   }
 }
